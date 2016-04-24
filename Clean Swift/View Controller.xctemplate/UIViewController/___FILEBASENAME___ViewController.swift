31a3bde980197456f4e7e482532f99088d062ba6
Jakub Knejzlík <jakub.knejzlik@gmail.com>
2016-04-24 23:50:55 +0200

    indent 4 spaces

--- Clean Swift/View Controller.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
+++ Clean Swift/View Controller.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -13,51 +13,51 @@ import UIKit
 
 protocol ___FILEBASENAMEASIDENTIFIER___ViewControllerInput
 {
-  func displaySomething(viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel)
+    func displaySomething(viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel)
 }
 
 protocol ___FILEBASENAMEASIDENTIFIER___ViewControllerOutput
 {
-  func doSomething(request: ___FILEBASENAMEASIDENTIFIER___Request)
+    func doSomething(request: ___FILEBASENAMEASIDENTIFIER___Request)
 }
 
 class ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController, ___FILEBASENAMEASIDENTIFIER___ViewControllerInput
 {
-  var output: ___FILEBASENAMEASIDENTIFIER___ViewControllerOutput!
-  var router: ___FILEBASENAMEASIDENTIFIER___Router!
+    var output: ___FILEBASENAMEASIDENTIFIER___ViewControllerOutput!
+    var router: ___FILEBASENAMEASIDENTIFIER___Router!
   
-  // MARK: Object lifecycle
+    // MARK: Object lifecycle
   
-  override func awakeFromNib()
-  {
-    super.awakeFromNib()
-    ___FILEBASENAMEASIDENTIFIER___Configurator.sharedInstance.configure(self)
-  }
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        ___FILEBASENAMEASIDENTIFIER___Configurator.sharedInstance.configure(self)
+    }
   
-  // MARK: View lifecycle
+    // MARK: View lifecycle
   
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    doSomethingOnLoad()
-  }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        doSomethingOnLoad()
+    }
   
-  // MARK: Event handling
+    // MARK: Event handling
   
-  func doSomethingOnLoad()
-  {
-    // NOTE: Ask the Interactor to do some work
+    func doSomethingOnLoad()
+    {
+        // NOTE: Ask the Interactor to do some work
     
-    let request = ___FILEBASENAMEASIDENTIFIER___Request()
-    output.doSomething(request)
-  }
+        let request = ___FILEBASENAMEASIDENTIFIER___Request()
+        output.doSomething(request)
+    }
   
-  // MARK: Display logic
+    // MARK: Display logic
   
-  func displaySomething(viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel)
-  {
-    // NOTE: Display the result from the Presenter
+    func displaySomething(viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel)
+    {
+        // NOTE: Display the result from the Presenter
     
-    // nameTextField.text = viewModel.name
-  }
+        // nameTextField.text = viewModel.name
+    }
 }

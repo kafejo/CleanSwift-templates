5816914c194a047489a37e0c383bbc07fead0b59
Aleš Kocur <ales@thefuntasty.com>
2016-08-28 10:51:16 +0100

    Add Storyboard Handler Type template

---
+++ Clean Swift/Segue Handler Type.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+protocol SegueHandlerType {
+    associatedtype SegueIdentifier: RawRepresentable
+}
+
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+
+    func performSegueWithIdentifier(segueIdentifier: SegueIdentifier, sender: AnyObject?) {
+        performSegueWithIdentifier(segueIdentifier.rawValue, sender: sender)
+    }
+
+    func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
+        guard let identifier = segue.identifier, segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+            fatalError("Invalid segue identifier \(segue.identifier)")
+        }
+
+        return segueIdentifier
+    }
+}

b73a17333787f56ffcddcfa55df0c6e166cf9957
Ales Kocur <ales@rubicoin.com>
2016-09-19 10:32:31 +0100

    Quick Spec template for viewcontroller

---
+++ Clean Swift/Specs.xctemplate/___FILEBASENAME___InteractorSpec.swift
@@ -0,0 +1,46 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAME___
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___InteractorTests: XCTestCase {
+
+    // MARK: - Subject under test
+
+    var sut: ___FILEBASENAMEASIDENTIFIER___Interactor!
+
+    // MARK: - Test lifecycle
+
+    override func setUp() {
+        super.setUp()
+        setup___FILEBASENAMEASIDENTIFIER___Interactor()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: - Test setup
+
+    func setup___FILEBASENAMEASIDENTIFIER___Interactor() {
+        sut = ___FILEBASENAMEASIDENTIFIER___Interactor()
+    }
+
+    // MARK: - Test doubles
+
+    // MARK: - Tests
+
+    func testSomething() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
---
+++ Clean Swift/Specs.xctemplate/___FILEBASENAME___PresenterSpec.swift
@@ -0,0 +1,46 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+@testable import ___PROJECTNAME___
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___PresenterTests: XCTestCase {
+
+    // MARK: - Subject under test
+
+    var sut: ___FILEBASENAMEASIDENTIFIER___Presenter!
+
+    // MARK: - Test lifecycle
+
+    override func setUp() {
+        super.setUp()
+        setup___FILEBASENAMEASIDENTIFIER___Presenter()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    // MARK: - Test setup
+
+    func setup___FILEBASENAMEASIDENTIFIER___Presenter() {
+        sut = ___FILEBASENAMEASIDENTIFIER___Presenter()
+    }
+
+    // MARK: - Test doubles
+
+    // MARK: - Tests
+
+    func testSomething() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
